b10b4761866c59c8545ce186d207db0f542a31d0
Dan Da'neshgar <dan@lastsignal.com>
2016-01-19 13:39:40 -0600

    Initial commit

---
+++ Web.Api/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Web.Api.Controllers
+{
+    public class TestController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new {Id = 5, Value = "Five"});
+        }
+    }
+}
